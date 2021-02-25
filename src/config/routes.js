fa554d737c02ae017c77fa4aa96a67a4faa535de
paulinaludwig <paulina.ludwig@ui.com>
2021-02-25 18:09:50 +0100

    Add routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,12 @@
-const { getParams } = require('../util/util');
+const { getParams, getQuery } = require('../util/util');
 
 // TODO add more routes
 
 const Type = Object.freeze({
   GET: 'GET',
   PUT: 'PUT',
+  DELETE: 'DELETE',
+  POST: 'POST'
 });
 
 const Category = Object.freeze({
@@ -14,6 +16,7 @@ const Category = Object.freeze({
 });
 
 const routes = [
+  // Tickets
   {
     name: 'Update Ticket',
     category: Category.tickets,
@@ -22,6 +25,102 @@ const routes = [
     params: getParams('/api/v2/tickets/{ticket_id}'),
     automated: false,
   },
+  {
+    name: 'Delete Ticket',
+    category: Category.tickets,
+    type: Type.DELETE,
+    route: '/api/v2/tickets/{ticket_id}',
+    params: getParams('/api/v2/tickets/{ticket_id}'),
+    automated: false,
+  },
+  {
+    name: 'Delete Tickets',
+    category: Category.tickets,
+    type: Type.DELETE,
+    route: '/api/v2/tickets/destroy_many?ids={ids}',
+    params: getQuery('/api/v2/tickets/destroy_many?ids={ids}'),
+    automated: false,
+  },
+  {
+    name: 'Create Ticket',
+    category: Category.tickets,
+    type: Type.POST,
+    route: '/api/v2/tickets',
+    automated: false,
+  },
+  {
+    name: 'Get Ticket',
+    category: Category.tickets,
+    type: Type.GET,
+    route: '/api/v2/tickets/{ticket_id}',
+    params: getParams('/api/v2/tickets/{ticket_id}'),
+    automated: false,
+  },
+  // Users 
+  {
+    name: 'Update User',
+    category: Category.users,
+    type: Type.PUT,
+    route: '/api/v2/users/{user_id}',
+    params: getParams('/api/v2/users/{user_id}'),
+    automated: false,
+  },
+  {
+    name: 'Delete User',
+    category: Category.users,
+    type: Type.DELETE,
+    route: '/api/v2/users/{user_id}',
+    params: getParams('/api/v2/users/{user_id}'),
+    automated: false,
+  },
+  {
+    name: 'Create User',
+    category: Category.users,
+    type: Type.POST,
+    route: '/api/v2/users',
+    automated: false,
+  },
+  {
+    name: 'Get User',
+    category: Category.users,
+    type: Type.GET,
+    route: '/api/v2/users/{user_id}',
+    params: getParams('/api/v2/users/{user_id}'),
+    automated: false,
+  },
+  // Articles
+  {
+    name: 'Update Article',
+    category: Category.articles,
+    type: Type.PUT,
+    route: '/api/v2/help_center/en-us/articles/{article_id}',
+    params: getParams('/api/v2/help_center/en-us/articles/{article_id}'),
+    automated: false,
+  },
+  {
+    name: 'Delete article',
+    category: Category.articles,
+    type: Type.DELETE,
+    route: '/api/v2/help_center/en-us/articles',
+    params: getParams('/api/v2/help_center/en-us/articles/{article_id}'),
+    automated: false,
+  },
+  {
+    name: 'Create Article',
+    category: Category.articles,
+    type: Type.POST,
+    route: '/api/v2/articles',
+    automated: false,
+  },
+  {
+    name: 'Get Article',
+    category: Category.articles,
+    type: Type.GET,
+    route: '/api/v2/articles/{article_id}',
+    params: getParams('/api/v2/articles/{article_id}'),
+    automated: false,
+  },
+  // Automations 
   {
     name: '',
     category: '',
